5c72c650108345a86c4a9f3e11f0263dae9ae6ae
beiyuanfuyun <109722550+beiyuanfuyun@users.noreply.github.com>
2022-08-09 18:49:04 +0800

    Add files via upload

---
+++ c源0.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>//打开工具箱，里面的函数随意使用
+#include<windows.h>
+int main()//圆括号//Mian-主函数,说出去的话，从这里开始且并只有一个
+{
+	//计算机讲的话
+	printf("hello  world\n");//计算机以;号作为一句简单话的结束
+	//调试一闪而过，卡住控制窗口，可以利用getchar()函数，等待输入一个数据
+	//或者使用时间滞后函数system()但要插入新的工具箱#include<windows.h>
+	//system("pause");
+	//system("pause");
+	
+	return 0;//告诉计算机讲完了
+}
+//.exe表示可执行文件
+//注释：   /斜杠  \反斜杠    //单行注释   /**/多行注释
